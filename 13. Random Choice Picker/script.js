d7b7a4bfd524da041dd23550f086cb8fbaa86ce0
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2021-10-20 03:13:45 +0530

    Add Project 13
    Completed Day-13 Project Random Choice Picker, Completed the Random Pick Function in JS

--- 13. Random Choice Picker/script.js
+++ 13. Random Choice Picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value);
+
+    if(e.key=== 'Enter') {
+        setTimeout(() => {
+            e.target.value= '';
+        },10);
+
+        randomSelect();
+    }
 });
 
 function createTags(input) {
@@ -20,4 +28,43 @@ function createTags(input) {
         // tagEL.innerText = tag;
         // tagsEl.appendChild(tagEL);
     })
+}
+
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
