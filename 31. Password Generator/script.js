76de27e4f6941e307aee8dd0a840948f779fa853
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2021-10-31 23:39:08 +0530

    Add Password Generate Function

--- 31. Password Generator/script.js
+++ 31. Password Generator/script.js
@@ -1,3 +1,12 @@
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -5,16 +14,44 @@ const randomFunc = {
     symbol: getRandomSymbol,
 };
 
+generateEl.addEventListener("click", () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = "";
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter((item) => Object.values(item)[0]);
+
+    if (typesCount === 0) {
+        return "";
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach((type) => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    return generatedPassword.slice(0, length);
+}
+
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random * 26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random * 10) + 48);
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
